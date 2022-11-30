1d4badc3ccd3056f955d6328224a3ca94334160d
koushiksridhar <koushiksridhar02@gmail.com>
2022-11-30 00:27:59 -0500

    redo with autograder

--- bin/cli.js
+++ bin/cli.js
@@ -3,11 +3,11 @@
 import minimist from "minimist"
 import { roll } from "../lib/roll.js"
 
-const args = minimist(process.argv.slice(2))
+const args = minimist(process.argv.slice(2));
 
-const sides = args.sides || 6
-const dice = args.dice || 2
-const rolls = args.rolls || 1
-const results = roll(sides, dice, rolls)
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+const results = roll(sides, dice, rolls);
 
-console.log(results)
+console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
